ea30219c8d1a16740246f4ac49ffc7fa90f5c0e9
Miguel Yann <tsambamiguelito@gmail.com>
2020-06-17 23:57:34 +0200

    Should print Hello

---
+++ src/main/java/fr/myt/learn/roadtoxp/RoadtoxpApplication.java
@@ -0,0 +1,13 @@
+package fr.myt.learn.roadtoxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadtoxpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadtoxpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/myt/learn/roadtoxp/controllers/HelloTest.java
@@ -0,0 +1,32 @@
+package fr.myt.learn.roadtoxp.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void shouldPrintHello() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .get("/hello"))
+
+                .andExpect(status()
+                        .isOk())
+                .andExpect(content().string("Hello ROAD PROJECT :)"));
+
+    }
+}
